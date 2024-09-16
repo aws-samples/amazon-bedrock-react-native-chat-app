f4d85e37d8b14d8b0b2236409509364efb84352b
Deekshitulu Pentakota <pdeeksh@amazon.com>
2024-09-16 14:04:59 +0530

    added copyright info

--- amplify/functions/api-function/handler.ts
+++ amplify/functions/api-function/handler.ts
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
 import { BedrockRuntimeClient, InvokeModelCommand } from "@aws-sdk/client-bedrock-runtime";
 import { APIGatewayProxyHandler } from "aws-lambda";
 
